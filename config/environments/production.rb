83c9de725e9eac1099917bb4df27097038abe310
Julian O. B. Bangert '14 <julian@hacktown.cs.dartmouth.edu>
2012-02-14 01:25:04 -0500

    Finally fixed it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Lunchtown::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -57,4 +57,5 @@ Lunchtown::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.relative_url_root = "/valentines"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Lunchtown::Application.routes.draw do
-  devise_for :persons
-  resources :person
-  root :to => "person#index"
-  #get "/", :controller => :person , :action=> :index
-  #post "/", :controller =>  :person, :action => :update
+  scope ENV['RAILS_RELATIVE_URL_ROOT'] || '/' do
+    
+    devise_for :persons
+    resources :person
+    root :to => "person#index"
+    #get "/", :controller => :person , :action=> :index
+    #post "/", :controller =>  :person, :action => :update
+  end
 end
